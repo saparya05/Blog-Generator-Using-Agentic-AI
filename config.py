64271549e01cacb2bdf7a378a53662fbf761f041
saparya05 <saparya05@gmail.com>
2026-03-19 23:24:07 +0530

    fix groq api key

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 import os
+import streamlit as st
 
 load_dotenv()
 
 llm = ChatGroq(
     model="meta-llama/llama-4-scout-17b-16e-instruct",
-    api_key=os.getenv("GROQ_API_KEY")
+    api_key = os.getenv("GROQ_API_KEY") or st.secrets["GROQ_API_KEY"]
 )
